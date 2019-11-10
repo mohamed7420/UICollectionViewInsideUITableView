185e8e4da95b85f640ab178f3b05acc45cf1ffef
mohamedosama2074 <iosengks74@gmail.com>
2019-11-10 22:32:01 +0200

    Initial Commit

---
+++ UICollectionViewInsideUITableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UICollectionViewInsideUITableView
+//
+//  Created by Mohamed on 11/10/19.
+//  Copyright © 2019 Mohamed74. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
